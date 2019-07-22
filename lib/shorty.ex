65cae6a95007ec8e28f86b8d3d1aa446491840ae
Michael Teter <m@michaelteter.com>
2019-07-22 20:33:43 +0200

    basic implementation of url shortener

--- lib/shorty.ex
+++ lib/shorty.ex
@@ -3,7 +3,7 @@ defmodule Shorty do
 
   # --- API ---
 
-  def start_server(initial_state \\ %{}) do
+  def start_server(initial_state \\ nil) do
     GenServer.start_link(__MODULE__, initial_state)
   end
 
@@ -11,18 +11,31 @@ defmodule Shorty do
     GenServer.call(pid, {:get_state})
   end
 
+  def shorten(pid, url) do
+    GenServer.call(pid, {:shorten, url})
+  end
+
   # --- Server ---
 
-  def init(), do: {:ok, new_state()}
+  def init(nil), do: {:ok, new_state()}
   def init(initial_state), do: {:ok, initial_state}
 
   def handle_call({:get_state}, _from, state) do
     {:reply, state, state}
   end
 
+  def handle_call({:shorten, url}, _from, state) do
+    url_id = Enum.count(state[:urls_by_id]) + 1
+    state = put_in(state, [:urls_by_id, url_id], url)
+    {:reply, url_id, state}
+  end
+
   # --- Util ---
 
   def new_state() do
-    %{}
+    %{urls_by_id: %{},
+      # urls: %{},
+      # urls_by_hostname: %{}
+    }
   end
 end
--- test/shorty_test.exs
+++ test/shorty_test.exs
@@ -11,4 +11,14 @@ defmodule ShortyTest do
     assert {:ok, pid} = Shorty.start_server(initial_state)
     assert initial_state = Shorty.get_state(pid)
   end
+
+  test "shortens a URL" do
+    {:ok, pid} = Shorty.start_server()
+    url1 = "https://example.com/foo"
+    assert 1 = Shorty.shorten(pid, url1)
+    url2 = "https://example.com/bar"
+    assert 2 = Shorty.shorten(pid, url2)
+    assert %{urls_by_id: %{1 => url1,
+                           2 => url2}} == Shorty.get_state(pid)
+  end
 end
