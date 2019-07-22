f0ae6e26a2dbdd35bb36d9dd7dab0789b6bb6a9b
Michael Teter <m@michaelteter.com>
2019-07-22 20:21:01 +0200

    added initial state capability

--- lib/shorty.ex
+++ lib/shorty.ex
@@ -7,10 +7,18 @@ defmodule Shorty do
     GenServer.start_link(__MODULE__, initial_state)
   end
 
+  def get_state(pid) do
+    GenServer.call(pid, {:get_state})
+  end
+
   # --- Server ---
 
   def init(), do: {:ok, new_state()}
   def init(initial_state), do: {:ok, initial_state}
+
+  def handle_call({:get_state}, _from, state) do
+    {:reply, state, state}
+  end
 
   # --- Util ---
 
--- test/shorty_test.exs
+++ test/shorty_test.exs
@@ -5,4 +5,10 @@ defmodule ShortyTest do
   test "starts a server with no state" do
     assert {:ok, _pid} = Shorty.start_server()
   end
+
+  test "starts a server with state" do
+    initial_state = %{sample: "data"}
+    assert {:ok, pid} = Shorty.start_server(initial_state)
+    assert initial_state = Shorty.get_state(pid)
+  end
 end
